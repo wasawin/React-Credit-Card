fd1433160e331bd0964c313899a1e7c0aaab0aa6
wasawin <wasawin.14@gmail.com>
2024-08-16 21:22:43 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import CardFront from './components/CardFront';
 function App() {
   return (
     <>
-      <main className=" flex justify-center items-center min-h-screen w-full   ">
-        <div className="border border-black p-4 rounded-xl bg-gradient-to-r from-pink-500 to-rose-500 w-full h-96 group [perspective:1000px] ">
+      <main className=" flex justify-center items-center min-h-screen w-full   p-2 md:p-24">
+        <div className="border border-black p-4 rounded-xl bg-gradient-to-r from-pink-500 to-rose-500 w-full md:w-96 h-96 group [perspective:1000px] overflow-hidden ">
           <article
             className="flex flex-col justify-center items-center  w-full h-full rounded-2xl  relative
           [transform-style:preserve-3d]  group-hover:[transform:rotateY(180deg)] transition-all duration-1000 [transform-origin:center_center]
