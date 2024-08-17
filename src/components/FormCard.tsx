7f6c1189d6a111bf907f86842c5b4816d16a0d1a
wasawin <wasawin.14@gmail.com>
2024-08-18 00:53:45 +0700

    add input components

--- src/components/FormCard.tsx
+++ src/components/FormCard.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
+import InputCard from './InputCard';
 
 function FormCard() {
   return (
     <>
       <div className="bg-white  h-96 w-full">
-        <label htmlFor="Cardnumber">
-          <input
+        <h1>Detail for Card</h1>
+        <section className="flex flex-col justify-center items-center gap-4 p-3 ">
+          <InputCard
             type="text"
-            placeholder="Card number"
-            className="w-full border border-black"
+            placholder="Card number"
             inputMode="numeric"
+            minLength={16}
+            maxLength={16}
             pattern="[0-9]*"
-            maxLength={3}
           />
-        </label>
+          <InputCard type="text" placholder="Card holder" pattern="[a-zA-Z]*" />
+          <div className="flex gap-4">
+            <InputCard
+              type="text"
+              inputMode="numeric"
+              placholder="Month"
+              minLength={2}
+              maxLength={2}
+              pattern="[0-9]*"
+            />
+            <InputCard
+              type="text"
+              inputMode="numeric"
+              placholder="Year"
+              minLength={2}
+              maxLength={2}
+              pattern="[0-9]*"
+            />
+          </div>
+          <InputCard
+            type="text"
+            inputMode="numeric"
+            placholder="CVC"
+            minLength={3}
+            maxLength={3}
+            pattern="[0-9]*"
+          />
+        </section>
       </div>
     </>
   );
---
+++ src/components/InputCard.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+interface InputCardProps {
+  type: 'text' | 'tel' | 'date' | 'email' | 'number' | 'date' | 'month';
+  placholder: string;
+  inputMode?:
+    | 'text'
+    | 'numeric'
+    | 'decimal'
+    | 'tel'
+    | 'search'
+    | 'email'
+    | 'url';
+  pattern?: '[0-9]*' | '[a-zA-Z]*' | '[a-zA-Z0-9]*';
+  minLength?: number;
+  maxLength?: number;
+}
+
+const InputCard: React.FC<InputCardProps> = ({
+  type,
+  placholder,
+  inputMode,
+  pattern,
+  minLength,
+  maxLength,
+}) => {
+  return (
+    <label
+      htmlFor={`User${placholder}`}
+      className="relative block w-full overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+    >
+      <input
+        type={type}
+        id={`User${placholder}`}
+        placeholder={placholder}
+        inputMode={inputMode}
+        pattern={pattern}
+        minLength={minLength}
+        maxLength={maxLength}
+        className="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+      />
+
+      <span className="absolute start-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+        {placholder}
+      </span>
+    </label>
+  );
+};
+
+export default InputCard;
